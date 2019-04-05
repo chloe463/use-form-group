bc282426e932917d1b85e92b0ac0b9e038683b8a
TsuyoshiHara <chloe463@gmail.com>
2019-04-05 23:32:18 +0900

    fix(Validators): Fix validator functions

--- src/lib/react-form-control/Validators.ts
+++ src/lib/react-form-control/Validators.ts
@@ -1,4 +1,4 @@
-export type ValidatorFn = (value: any) => boolean;
+export type ValidatorFn = (value: any) => ValidatorErrors | null;
 export type ValidatorPromiseFn = (value: any) => Promise<boolean>;
 export type Validator = ValidatorFn | ValidatorPromiseFn;
 export type ValidatorErrors = {
@@ -7,12 +7,30 @@ export type ValidatorErrors = {
 
 export class Validators {
   static required: ValidatorFn = (value: any) => {
-    if (value === null && value === undefined) {
-      return false;
+    if (value === null || value === undefined) {
+      return { "required": true };
     }
-    if (typeof value === "string") {
-      return value !== "";
+    return null;
+  }
+
+  static maxLength = (length: number): ValidatorFn => (value: string) => {
+    if (value.length >= length) {
+      return { "maxLength": true };
     }
-    return true;
+    return null;
+  }
+
+  static minLength = (length: number): ValidatorFn => (value: string) => {
+    if (value.length <= length) {
+      return { "minLength": true };
+    }
+    return null;
+  }
+
+  static format = (regexp: RegExp): ValidatorFn => (value: string) => {
+    if (value.match(regexp)) {
+      return { "format": true };
+    }
+    return null;
   }
 }
