df3b724ece5da8e7283a8ef7efdb42564f0fb460
TsuyoshiHara <chloe463@gmail.com>
2019-04-07 01:15:26 +0900

    refactor(FieldGroup): Use functional component and delete wasted codes

--- src/lib/react-form-control/FieldGroup.tsx
+++ src/lib/react-form-control/FieldGroup.tsx
@@ -1,37 +1,18 @@
-import React, { Component } from "react";
+import React  from "react";
 
 import { FieldContext } from "./FieldContext";
 import { FormGroup } from "./FormGroup";
-import { FormControl } from "./FormControl";
 
 interface FieldGroupProps {
   formGroup: FormGroup;
   render: React.FunctionComponent<any>;
 }
-interface FieldGroupState {
-  [key: string]: any;
-}
 
-
-export class FieldGroup extends Component<FieldGroupProps, FieldGroupState> {
-  constructor(props: FieldGroupProps) {
-    super(props);
-    this.state = {};
-  }
-
-  componentDidMount() {
-    Object.keys(this.props.formGroup.controls).forEach((name: string) => {
-      const control = this.props.formGroup.controls[name];
-      this.setState({ [name]: control.value });
-    });
-  }
-
-  public render() {
-    const { formGroup, render } = this.props;
-    return (
-      <FieldContext.Provider value={{formGroup}}>
-        {render({})}
-      </FieldContext.Provider>
-    );
-  }
-}
+export const FieldGroup: React.FC<FieldGroupProps> = (props: FieldGroupProps) => {
+  const { formGroup, render } = props;
+  return (
+    <FieldContext.Provider value={{formGroup}}>
+      {render({})}
+    </FieldContext.Provider>
+  );
+};
