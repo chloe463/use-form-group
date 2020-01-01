dd69105e965bf29d66f3d2010d97f0b3214dd320
TsuyoshiHara <chloe463@gmail.com>
2020-01-01 17:03:54 +0900

    test: Use '@testing-library/react-hooks' instead of 'react-hooks-testing-library'

--- src/lib/react-form-control/__tests__/useFormGroup.test.tsx
+++ src/lib/react-form-control/__tests__/useFormGroup.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook, act } from "react-hooks-testing-library";
+import { renderHook, act } from "@testing-library/react-hooks";
 import { initMeta, initValidators, useFormGroup } from "../useFormGroup";
 import { Validators } from "../Validators";
 
